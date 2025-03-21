b3592362ee80756daf8f0d08492fe6b091e092b5
pradnya <pradnyap0006@gmail.com>
2025-03-21 11:26:13 +0530

    updates of day2

---
+++ day-2/variable.tf
@@ -0,0 +1,16 @@
+variable "ami" {
+description = "ami values inserting"
+type = string
+default = ""
+}
+variable "instance_type" {
+    description = "instance type values"
+    type = string
+    default = ""
+}
+variable "key_name" {
+    description = "key name values"
+    type = string
+  default = ""
+  
+}
